9ab3409d1b9d259549bd40ee036ca04d9c97ab72
GuidoAsplanatti <guidoasplanatti@gmail.com>
2024-02-07 12:34:43 -0300

    switch case for pages php

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <?php include('templates/head.html'); ?>
-<?php include('templates/webpages/home.html'); ?>
+
+<?php
+// Definir una variable para la página actual
+$page = isset($_GET['page']) ? $_GET['page'] : 'home';
+?>
+
+<body>
+    <?php
+    // Cargar el contenido de la página correspondiente
+    switch ($page) {
+        case 'home':
+            include 'templates/webpages/home.html';
+            break;
+        case 'proyectos':
+            include 'templates/webpages/proyectos.html';
+            break;
+        case 'contacto':
+            include 'contacto.php';
+            break;
+        // Agregar más casos según las páginas que tengas
+        default:
+            include 'home.php'; // Página predeterminada
+            break;
+    }
+    ?>
+</body>
 
 <?php include('templates/footer.html'); ?>
 
