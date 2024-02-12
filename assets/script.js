0326f2c917c1c70ff783912de792404e2afc9eac
GuidoAsplanatti <guidoasplanatti@gmail.com>
2024-02-11 22:29:28 -0300

    Arrangements

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,3 @@
-// function iniciarMap(){
-//     var coord = {lat:-31.39141707170465 ,lng: -58.017525273008715};
-//     var map = new google.maps.Map(document.getElementById('map'),{
-//       zoom: 15,
-//       center: coord
-//     });
-//     var marker = new google.maps.Marker({
-//       position: coord,
-//       map: map
-//     });
-// }
-
 function onLoadMap() {
     var map = new Microsoft.Maps.Map(document.getElementById('mapContainer'), {
         credentials: 'ApoYMlspwO6CAraKKDGz3BnLGxZzp3_TAJwB2dzrbj_4OjvffvMH6vY7f05i4jCl',
