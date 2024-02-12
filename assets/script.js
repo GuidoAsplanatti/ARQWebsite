354c3050ce6901651c900c94b4aac98dbf01c146
GuidoAsplanatti <guidoasplanatti@gmail.com>
2024-02-12 00:25:50 -0300

    ,for.,

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 function onLoadMap() {
     var map = new Microsoft.Maps.Map(document.getElementById('mapContainer'), {
         credentials: 'ApoYMlspwO6CAraKKDGz3BnLGxZzp3_TAJwB2dzrbj_4OjvffvMH6vY7f05i4jCl',
-        center: new Microsoft.Maps.Location(-31,391450, -58,017508), // Latitud y longitud de la ubicación
+        center: new Microsoft.Maps.Location(-31.391450, -58.017508), // Latitud y longitud de la ubicación
         zoom: 10 // Nivel de zoom inicial
     });
 }
