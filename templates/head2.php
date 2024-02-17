5bb74d9657d40e4503357208e584602ba7b34516
GuidoAsplanatti <guidoasplanatti@gmail.com>
2024-02-16 22:53:59 -0300

    fixing head nav active

--- templates/head2.php
+++ templates/head2.php
@@ -12,10 +12,6 @@ $pages = array(
 );
 ?>
 
-<head>
-  <link rel="icon" href="assets/ARQLogoPestania.ico" type="image/x-icon" />
-</head>
-
 <header class="head-container">
   <div class="container">
       <div class="d-flex flex-wrap justify-content-center py-4">
@@ -25,12 +21,11 @@ $pages = array(
   
         <ul class="nav nav-underline nav-scroller mb-3">
           <?php foreach ($pages as $page => $label): ?>
-            <li class="nav-item <?php echo ($current_page == $page) ? 'active' : ''; ?>">
-              <a href="index.php?page=<?php echo $page; ?>" class="nav-link link-body-emphasis"><?php echo $label; ?></a>
+            <li class="nav-item">
+              <a href="index.php?page=<?php echo $page; ?>" class="nav-link link-body-emphasis <?php echo ($current_page == $page) ? 'active' : ''; ?>"><?php echo $label; ?></a>
             </li>
           <?php endforeach; ?>
         </ul>
       </div>
   </div>
-  
 </header>
